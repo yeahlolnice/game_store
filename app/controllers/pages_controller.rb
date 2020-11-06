9913bda2d5be10fdc3872c4d895eb0e61ea676ff
Matthew Williams <mattwilly98@hotmail.com>
2020-11-06 14:31:47 +1100

    rolify and default roles assigned

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,22 @@
 class PagesController < ApplicationController
+  before_action :authenticate_user!, except: [:home, :profile]
+  before_action :set_profile, only: [:profile, :library]
   def home
+    
+  end
+  
+  def library
+    
+  end
+
+  def profile
+    
+  end
+
+  
+  private
+  
+  def set_profile
+    @user = User.find(params[:user_id])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ApplicationRecord
+  rolify
+  after_create :assign_default_role
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :timeoutable
+
+  validates :username, :email, presence: true, uniqueness: true
+
+  def assign_default_role
+    self.add_role(:public) if self.roles.blank?
+  end
 end
